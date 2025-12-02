b3a24d250b308dcc806822b6407c576476e1761b
C H <hedges.colin@gmail.com>
2025-12-01 22:51:20 -0800

    Updated cli container and README

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -6,18 +6,10 @@
 # remove net-tools, as it conflicts with uutils
 apk del net-tools
 
-# remove the system-wide zshrc configs
-sed -i '/STARSHIP_CONFIG/,/starship/d' /etc/zsh/zshrc
-
 # fix permissions
 chmod 0775 /var/mail
 
 # Update the container and install packages
 apk update && apk upgrade
 grep -v '^#' ./cli.packages | xargs apk add
-
-# make the trigger for setting up linuxbrew more sensitive
-sed -i 's/test ! -d \/home\/linuxbrew\/\.linuxbrew/test ! -d \/home\/linuxbrew\/\.linuxbrew\/bin/g' /etc/profile.d/00-bluefin-cli-brew-firstrun.sh
-
-# remove the find=fd alias, as fd syntax is incompatible with find
-sed -i '/fd/d' /etc/profile.d/modern-unix.sh
+mv /home/linuxbrew /home/homebrew
