5e06ba71f2ab9f75e5173b24e308540cb99362dd
C H <hedges.colin@gmail.com>
2026-01-22 11:47:22 -0800

    Added qemu and bcvk to symlinked host binaries in distrobox-shims

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -8,3 +8,5 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/buildah
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/distrobox
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/qemu
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/bcvk
