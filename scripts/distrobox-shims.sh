dfef2ea2b5cc7516229664894e3d84aac1f5d4fb
C H <hedges.colin@gmail.com>
2026-01-22 16:17:43 -0800

    Moved the bcvk shim to /usr/bin rather than /usr/local/bin due to shenanigans with the host build

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -9,4 +9,4 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/distrobox
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/qemu
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/bcvk
+ln -fs /usr/bin/distrobox-host-exec /usr/bin/bcvk
